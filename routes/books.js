7ff6ee465f75f4e5762ed2004f194c902e480591
Tim <tjcutt@gmail.com>
2017-03-29 16:38:45 -0600

    finished part 3 without bonus

--- routes/books.js
+++ routes/books.js
@@ -1,10 +1,74 @@
 'use strict';
 
 const express = require('express');
-
-// eslint-disable-next-line new-cap
+const knex = require('../knex');
+const humps = require('humps');
 const router = express.Router();
 
-// YOUR CODE HERE
+router.get('/books', (req, res, next) => {
+  knex('books')
+    .orderBy('title')
+    .then(books => {
+      res.send(humps.camelizeKeys(books));
+    });
+  });
+
+router.get('/books/:id', (req, res, next) => {
+  knex('books')
+    .where('id', req.params.id)
+    .then(books => {
+      res.send(humps.camelizeKeys(books[0]));
+    });
+});
+
+router.post('/books', (req, res, next) => {
+  knex('books')
+    .returning(['id', 'title', 'author', 'genre', 'description', 'cover_url'])
+    .insert({title: req.body.title,
+      author: req.body.author,
+      genre: req.body.genre,
+      description: req.body.description,
+      cover_url: req.body.coverUrl
+    })
+    .then(book => {
+      res.send(humps.camelizeKeys(book[0]));
+    });
+})
+
+router.patch('/books/:id', (req, res, next) => {
+  let id = req.params.id
+  knex('books')
+     .where('id', id)
+     .returning(['id', 'title', 'author', 'genre', 'description', 'cover_url'])
+     .update({title: req.body.title,
+       author: req.body.author,
+       genre: req.body.genre,
+       description: req.body.description,
+       cover_url: req.body.coverUrl
+     })
+    .then(book => {
+      res.send(humps.camelizeKeys(book[0]));
+   });
+});
+
+router.delete('/books/:id', (req, res, next) => {
+  knex('books')
+    .returning(['title', 'author', 'genre', 'description', 'cover_url'])
+    .where('id', req.params.id)
+    .del()
+    .then(book => {
+      res.send(humps.camelizeKeys(book[0]));
+    });
+});
+
+//=================== BONUS
+
+// router.get('/books/:id', (req, res, next) => {
+//   knex('books')
+//      .whereNot('id', req.params.id)
+//      .then(()=>{
+//        res.redirect('../public/error')
+//      });
+// })
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,30 @@
 'use strict';
 
 const express = require('express');
-
-// eslint-disable-next-line new-cap
 const router = express.Router();
+const bcrypt = require('bcrypt-as-promised');
+const knex = require('../knex');
+const humps = require('humps');
 
-// YOUR CODE HERE
+router.post('/users', (req, res, next) => {
+  bcrypt.hash(req.body.password, 12)
+    .then((hashed_password) => {
+      return knex('users')
+        .insert({
+          first_name: req.body.firstName,
+          last_name: req.body.lastName,
+          email: req.body.email,
+          hashed_password: hashed_password
+        }, '*');
+    })
+    .then((users) => {
+      const user = users[0];
+      delete user.hashed_password;
+      res.send(humps.camelizeKeys(user));
+    })
+    .catch((err) => {
+      next(err);
+    });
+});
 
 module.exports = router;
