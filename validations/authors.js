5acafa0efbae86caf170f1df3d2233a6c0a34d42
Tim <tjcutt@gmail.com>
2017-04-05 19:08:56 -0600

    tim dotenv

---
+++ validations/authors.js
@@ -0,0 +1,17 @@
+'use strict';
+
+module.exports.post = {
+  body: {
+    email: Joi.string()
+      .label('Email')
+      .required()
+      .email()
+      .trim(),
+
+    password: Joi.string()
+      .label('Password')
+      .required()
+      .trim()
+      .min(8)
+  }
+};
